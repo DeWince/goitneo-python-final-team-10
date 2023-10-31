4942c4db8b60bdba2cd769d42e6dc20b2f0f6b67
Vladislav Pysarenko <vlad@pysarenko.com>
2023-10-31 17:42:09 +0100

    Revert fields.py

--- Contacts/fields.py
+++ Contacts/fields.py
@@ -4,14 +4,14 @@ from Errors.error_handlers import FormatError
 
 class Field:
     def __init__(self, value):
-        self.value = str(value)
+        self.value = value
 
     def __str__(self):
         return str(self.value)
     
-class Validatable(Field):
-    def is_valid_type(self, value):
-        return re.fullmatch(self.regex, value)
+class Validatable:
+    def is_valid_type(self, value, regex):
+        return re.match(regex, value)
 
 
 class Name(Field):
@@ -20,43 +20,26 @@ class Name(Field):
 class Address(Field):
     pass
 
-class Birthday(Validatable):
-    regex = r"\d{2}.\d{2}.\d{4}"
-    str_format = "%d.%m.%Y"
-    
-    def __init__(self, value):
-        if not super().is_valid_type(value):
-            raise FormatError("Incorrect date format, use format DD.MM.YYYY")
-
-        try:
-            self.value = datetime.strftime(
-                datetime.strptime(
-                    value, 
-                    self.str_format
-                ),
-                self.str_format
-            )
-        except Exception as e:
-            raise ValueError("Дата не існує")
-        # end try
-        # super().__init__(value)
+class Birthday(Field, Validatable):
+    def __init__(self, value, regex):
+        self.regex = regex
+        if not super().is_valid_type(value, regex):
+            raise FormatError("Not correct date, use format DD.MM.YYYY")
+        super().__init__(value)
     
     def to_datetime(self):
-        return datetime.strptime(self.value, self.str_format)
+        return datetime.strptime(self.value, '%d.%m.%Y')
         
     def __str__(self):
-        return str(self.value)
+        return str(f"{self.value.day}.{self.value.month}.{self.value.year}")
 
-class Phone(Validatable):
-    regex = r"\d{10}"
-    def __init__(self, value):
-        if not super().is_valid_type(value):
-            raise FormatError("Incorrect phone number format, should contain 10 digits")
+class Phone(Field, Validatable):
+    def __init__(self, value, regex):
+        self.regex = regex
+        if not super().is_valid_type(value, regex):
+            raise FormatError("Not correct number, should contain 10 digits")
         super().__init__(value)
 
-class Email(Validatable):
-    regex = r".+"
-    def __init__(self, value):
-        if not super().is_valid_type(value):
-            raise FormatError("Incorrect email format, should _____")
-        super().__init__(value)
+class Email(Field, Validatable):
+    def __init__(self, value, regex):
+        pass
