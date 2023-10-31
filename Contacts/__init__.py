57ef6d93a77b1b5f3c6b4074d46d059aaa3a4897
Vladislav Pysarenko <vlad@pysarenko.com>
2023-11-01 00:43:32 +0100

    fix import error

--- Contacts/__init__.py
+++ Contacts/__init__.py
@@ -1,6 +1,6 @@
-from Contacts.contacts import Contacts
-from Contacts.record import Record
 from Contacts.fields import Name, Address, Birthday, Phone, Email
+from Contacts.record import Record
+from Contacts.contacts import Contacts
 
 __all__ = [
     "Contacts",
