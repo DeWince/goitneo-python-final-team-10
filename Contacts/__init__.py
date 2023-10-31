32c7e39a2f45c480ce420fda8bb74cdd3972b67e
Vladislav Pysarenko <vlad@pysarenko.com>
2023-11-01 00:21:55 +0100

    add _INIT_ files

---
+++ Contacts/__init__.py
@@ -0,0 +1,13 @@
+from Contacts.contacts import Contacts
+from Contacts.record import Record
+from Contacts.fields import Name, Address, Birthday, Phone, Email
+
+__all__ = [
+    "Contacts",
+    "Record",
+    "Name",
+    "Address",
+    "Birthday",
+    "Phone",
+    "Email"
+]
