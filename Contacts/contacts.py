0a6d6f22e2008f31f931ed9286f644d1dbf64a4a
Vladislav Pysarenko <vlad@pysarenko.com>
2023-10-31 11:40:23 +0100

    add boilerplace tode

--- Contacts/contacts.py
+++ Contacts/contacts.py
@@ -1,59 +1,136 @@
 from collections import UserDict
 from datetime import datetime
+from Contacts.record import Record
 
 
 class Contacts(UserDict):
-    def add_record(self, record):
-        self.data[record.name.value] = record
+    def __init__(self, **config):
+        filename = config.get("filename")
+        self.data = UserDict()
+        # self.database_connector = FileWriter(filename)
+    # end def
 
-    def find(self, name):
-        return self.data[name]
-    
-
-    def delete(self, name):
-        if name in self.data:
-            del self.data[name]
-    
     def save(self):
-        pass
+        self.database_connector.save(self.data)
+    # end def
 
-    def load(self, filename='phonebook.bin'):
-        pass
+    def load(self):
+        self.data = UserDict(self.database_connector.load())
+    # end def
     
-    def get_birthdays(self, days):
-        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        result = {day : [] for day in days}
-        records = []
-        today = datetime.today().date()
+    def add_record(self, record: Record): 
+        if self.data.get(record.name, None):
+            raise KeyError(f"{record.name} вже існує")
+        # end if
+        
+        self.data[record.name] = record
+        self.save()
+        return record
+    # end def
+    
+    def find_record(self, needle: str):
+        results = list()
+        for contact in self.data:
+            if f"{contact!r}".find(needle) != -1:
+                results.append(contact)
+            # end if
+        # end for
+    # end def
+    
+    def get_record(self, needle: str):
+        results = self.find_record(needle)
+        if len(results) == 0:
+            raise KeyError("Нічого не знайдено")
+        elif len(results) > 1:
+            raise KeyError("Знайдено забагато результатів")
+        # end if
+        
+        return results[0]
+    # end def
+    
+    def delete_record(self, name: str):
+        try:
+            res = self.data.pop(name)
+            self.save()
+            return res
+        except:
+            raise KeyError("Контакт не знайдено")
+        # end try
+    # end def
+    
+    def get_birthdays(self, days: int = 7):
+        days = int(days)
+        current_date = datetime.now()
 
-        for record in self.data.values():
-            if(record.birthday == None):
+        next_birthdays = list()
+        for record in list(self.data.values()):
+            celebrate_day = record.get_celebrate_date(current_date.year)
+            if celebrate_day == None:
                 continue
+            # end if
 
-            birthday = record.birthday.to_datetime().date()
+            if (celebrate_day > current_date and (celebrate_day - current_date).days <= days):
+                next_birthdays.append(record)
+            # end if
+        # end for
+        return next_birthdays
+    # end def
+# end class
+
+
+
+
+    # def add_record(self, record):
+    #     self.data[record.name.value] = record
+
+    # def find(self, name):
+    #     return self.data[name]
+    
+
+    # def delete(self, name):
+    #     if name in self.data:
+    #         del self.data[name]
+    
+    # def save(self):
+    #     pass
+
+    # def load(self, filename='phonebook.bin'):
+    #     pass
+    
+    # def get_birthdays(self, days):
+    #     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    #     result = {day : [] for day in days}
+    #     records = []
+    #     today = datetime.today().date()
+
+    #     for record in self.data.values():
+    #         if(record.birthday == None):
+    #             continue
+
+    #         birthday = record.birthday.to_datetime().date()
             
-            birthday_this_year = birthday.replace(year = today.year) 
+    #         birthday_this_year = birthday.replace(year = today.year) 
 
-            if birthday_this_year < today:
-                birthday_this_year = birthday.replace(year = today.year + 1)
+    #         if birthday_this_year < today:
+    #             birthday_this_year = birthday.replace(year = today.year + 1)
 
-            day_of_week = birthday_this_year.weekday()  
-            delta_days = (birthday_this_year - today).days
+    #         day_of_week = birthday_this_year.weekday()  
+    #         delta_days = (birthday_this_year - today).days
 
-            if day_of_week == 5: 
-                day_of_week = 0
-                delta_days +=2
+    #         if day_of_week == 5: 
+    #             day_of_week = 0
+    #             delta_days +=2
 
-            if day_of_week == 6: 
-                day_of_week = 0
-                delta_days +=1
+    #         if day_of_week == 6: 
+    #             day_of_week = 0
+    #             delta_days +=1
 
-            day_name = days[day_of_week]
-            if delta_days < days:
-                result[day_name].append(record)
+    #         day_name = days[day_of_week]
+    #         if delta_days < days:
+    #             result[day_name].append(record)
 
-        for item in result:
-            if len(result[item]) > 0:
-                records.extend(result[item])
+    #     for item in result:
+    #         if len(result[item]) > 0:
+    #             records.extend(result[item])
 
-        return records
+    #     return records
--- Contacts/record.py
+++ Contacts/record.py
@@ -1,39 +1,223 @@
-from Contacts.fields import Birthday, Name, Phone
-
+import calendar
+from datetime import datetime
+from Contacts.fields import Birthday, Name, Phone, Address, Email
 
 class Record:
-    def __init__(self, name):
-        self.birthday = None
-        self.name = Name(name)
-        self.phones = []
+    fields = {
+        "name": "Контакт",
+        "address": "Адреса",
+        "emails": "Електронка",
+        "phones": "Телефон",
+        "birthday": "День народження"
+    }
+    sets = ["phones", "emails"]
 
-    def add_phone(self, phone):
-        self.phones.append(Phone(phone))
-
-    def change_phone(self, old_phone, new_phone):
-        pass
-
-    def clear_phone(self, phone):
-        pass
-
-    def add_mail(self, mail):
-        pass
-
-    def change_mail(self, old_mail, new_mail):
-        pass
-
-    def clear_mail(self, phone):
-        pass
-
-    def set_birthday(self, birthday):
-        self.birthday = Birthday(birthday)
-
-    def set_address(self, address):
-        pass 
-
-    def __rep__():
-        pass
+    def __init__(self, name: Name, address: Address = None, phones: set = set(), emails: set = set(), birthday: Birthday = None):
+        self.name = name if type(name) == Name else Name(name)
+        self.address = address if type(address) == Address else Address(address)
+        self.birthday = birthday if type(birthday) == Birthday else Birthday(birthday)
+        self.phones = set([phone if type(phone) == Phone else Phone(phone) for phone in phones])
+        self.emails = set([email if type(email) == Email else Email(email) for email in emails])
+    # end def
 
     def __str__(self):
-        phones_str = '; '.join(str(p) for p in self.phones)
-        return f"Contact name: {self.name}, birthday: {self.birthday}, phones: {phones_str}"
+        str = f"{self.name:>15}:   "
+        if self.birthday != None:
+            str += f"{self.fields['birthday']}: {self.birthday:<14}"
+        if len(self.phones) > 0:
+            str += f"{self.fields['phones']}: {'; '.join(self.phones)}"
+        if len(self.emails) > 0:
+            str += f"{self.fields['emails']}: {'; '.join(self.emails)}"
+        if self.address != None:
+            str += f"{self.fields['address']}: {self.address}"
+        # end if
+        return str
+    # end def
+    
+    def __repr__(self):
+        str = f"Record({self.name}, {self.address}, {self.phones}, {self.emails}, {self.birthday}"
+
+    def __getitem__(self, key):
+        if key in self.__dict__:
+            return self.__dict__[key]
+        # end if
+    # end def
+
+    def __setitem__(self, key, value):
+        if key in self.__dict__:
+            self.__dict__[key] = value
+        # end if
+    # end def
+    
+    def add_info(self, type, value):
+        if type in self.sets:
+            if value in self.__dict__[type]:
+                raise KeyError(f"{(self.fields[type])} вже існує")
+            # end if
+            
+            self.__dict__[type].add(value)
+        elif type in ["address", "birthday"]:
+            self.__dict__[type] = value
+        # end if
+    # end def
+    
+    def modify_info(self, type, old_value, new_value):
+        if type in self.sets:
+            self.delete_info(type, old_value)
+        # end if
+
+        self.add_info(type, new_value)
+        # end if
+    # end def
+    
+    def delete_info(self, type, value=None):
+        if type in self.sets:
+            try:
+                self.__dict__[type].remove(value)
+            except:
+                raise KeyError(f"{(self.fields[type])} не знайдено")
+            # end try
+        elif type == "address":
+            del self.__dict__[type]
+        # end if
+    # end def
+
+    def add_phone(self, phone):
+        self.add_info("phone", Phone(phone))
+    # end def
+
+    def change_phone(self, old_phone=None, new_phone=None):
+        if type(old_phone) == str:
+            old_phone = Phone(old_phone)
+        if type(new_phone) == str:
+            new_phone = Phone(new_phone)
+        # end if
+
+        self.modify_info("phones", old_phone, new_phone)
+        
+        return self
+    # end def
+
+    def delete_phone(self, phone):
+        if type(phone) == str:
+            phone = Phone(phone)
+        # end if
+
+        try:
+            self.delete_info("phones", phone)
+        except:
+            pass
+        # end try
+        
+        return self
+    # end def
+
+    def add_email(self, email):
+        self.add_info("email", Email(email))
+    # end def
+
+    def change_email(self, old_email=None, new_email=None):
+        if type(old_email) == str:
+            old_email = Email(old_email)
+        if type(new_email) == str:
+            new_email = Email(new_email)
+        # end if
+
+        self.modify_info("emails", old_email, new_email)
+        
+        return self
+    # end def
+
+    def delete_email(self, email):
+        if type(email) == str:
+            email = Email(email)
+        # end if
+
+        try:
+            self.delete_info("emails", email)
+        except:
+            pass
+        # end try
+
+        return self
+    # end def
+
+    def set_birthday(self, birthday):
+        if type(birthday) == str:
+            birthday = Birthday(birthday)
+        # end if
+        self.add_info("birthday", birthday)
+        
+        return self
+    # end def
+
+    def set_address(self, address):
+        if type(address) == str:
+            address = Address(address)
+        # end if
+        self.add_info("address", address)
+        
+        return self
+    # end def
+    
+    def get_celebrate_date(self, celebrate_year = None):
+        if self.birthday == None:
+            return
+        if celebrate_year == None:
+            celebrate_year = datetime.now().year
+        # end if
+
+        is_leap = calendar.isleap(celebrate_year)
+        bday = datetime.strptime(self.birthday, "%d.%m.%Y")
+        month = bday.month
+        day = bday.day
+        if (month == 2 and day == 29 and not is_leap):
+            day = 28
+        # end if
+
+        return datetime(
+            year=celebrate_year,
+            month=month,
+            day=day
+        ).date()
+    # end def
+# end class
+
+
+
+
+    # def __init__(self, name):
+    #     self.birthday = None
+    #     self.name = Name(name)
+    #     self.phones = []
+
+    # def add_phone(self, phone):
+    #     self.phones.append(Phone(phone))
+
+    # def change_phone(self, old_phone, new_phone):
+    #     pass
+
+    # def clear_phone(self, phone):
+    #     pass
+
+    # def add_mail(self, mail):
+    #     pass
+
+    # def change_mail(self, old_mail, new_mail):
+    #     pass
+
+    # def clear_mail(self, phone):
+    #     pass
+
+    # def set_birthday(self, birthday):
+    #     self.birthday = Birthday(birthday)
+
+    # def set_address(self, address):
+    #     pass 
+
+    # def __rep__():
+    #     pass
+
+    # def __str__(self):
+    #     phones_str = '; '.join(str(p) for p in self.phones)
+    #     return f"Contact name: {self.name}, birthday: {self.birthday}, phones: {phones_str}"
