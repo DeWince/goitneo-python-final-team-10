82c2c473e5c0d85fe6f0e2af93276f0faa96faa8
Vladislav Pysarenko <vlad@pysarenko.com>
2023-11-01 12:35:28 +0100

    autoformat

--- Contacts/fields.py
+++ Contacts/fields.py
@@ -2,27 +2,32 @@ import re
 from datetime import datetime
 from Errors.error_handlers import FormatError
 
+
 class Field:
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return str(self.value)
-    
+
     def __eq__(self, new):
         return str(self) == str(new)
-    
+
+
 class Validatable(Field):
     def is_valid_type(self, value, regex):
         return re.fullmatch(regex, value)
+
 
 class Name(Field):
     def __init__(self, value):
         self.value = value
 
+
 class Address(Field):
     def __init__(self, value):
         self.value = value
+
 
 class Birthday(Validatable):
     def __init__(self, value):
@@ -36,9 +41,10 @@ class Birthday(Validatable):
             super().__init__(date_formated)
         except:
             FormatError("Not correct date")
-    
+
     def to_datetime(self):
         return datetime.strptime(self.value, '%d.%m.%Y')
+
 
 class Phone(Validatable):
     def __init__(self, value):
@@ -46,6 +52,7 @@ class Phone(Validatable):
         if not super().is_valid_type(value, self.regex):
             raise FormatError("Not correct number, should contain 10 digits")
         super().__init__(value)
+
 
 class Email(Validatable):
     def __init__(self, value):
